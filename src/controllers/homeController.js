34904d2ceafaa17817a0c070b27f8301b75a5f22
Cesar David Millan <cesardavidmillan5@gmail.com>
2023-03-18 21:25:54 -0500

    Solving some bugs and modifying methods

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,1 +1,24 @@
 'use strict';
+import { products } from '../helpers/dataBase.js';
+import { pintarProductos } from '../helpers/productShowcase.js';
+
+//llamando a la fila
+let fila = document.getElementById('fila');
+
+//Pintar los productos de la tienda
+pintarProductos(products);
+
+//Escuchando clic en la fila contenedora de productos
+fila.addEventListener('click', function (evento) {
+  let datosProductoSeleccionado = {};
+  let tarjeta = evento.target.parentElement;
+
+  datosProductoSeleccionado.foto = tarjeta.querySelector('img').src;
+  datosProductoSeleccionado.nombre = tarjeta.querySelector('h3').textContent;
+  datosProductoSeleccionado.precio = tarjeta.querySelector('h5').textContent;
+
+  //usaremos la memoria del pc para guardar esta info
+  localStorage.setItem('producto', JSON.stringify(datosProductoSeleccionado));
+
+  window.location.href = './src/views/productinfo.html';
+});
--- src/helpers/dataBase.js
+++ src/helpers/dataBase.js
@@ -1,1 +1,50 @@
 'use strict';
+
+export let products = Array(
+  {
+    title: 'Get Programming with Node.js',
+    descripcion:
+      'Get Programming with Node.js teaches you to write server-side code in JavaScript using Node.js. In 37 fast-paced, fun, and practical lessons, you will discover how to extend your existing JavaScript skills to write back-end code for your web applications.',
+    fotos: [
+      'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/41-OvBpNo9L._SX397_BO1,204,203,200_.jpg',
+      'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/51jUHhpRUkL.jpg',
+    ],
+    price: 49.99,
+    rate: 4,
+  },
+  {
+    title:
+      'Express in Action: Writing, building, and testing Node.js applications',
+    descripcion:
+      'Express in Action is a carefully designed tutorial that teaches you how to build web applications using Node and Express.',
+    fotos: [
+      'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/71UYg8TlIsL.jpg',
+      'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/51I1C4kShgL.jpg',
+    ],
+    price: 39.99,
+    rate: 3.5,
+  },
+  {
+    title:
+      'Eloquent JavaScript, 3rd Edition: A Modern Introduction to Programming',
+    descripcion:
+      'Completely revised and updated, this best-selling introduction to programming in JavaScript focuses on writing real applications.',
+    fotos: [
+      'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/51InjRPaF7L._SX377_BO1,204,203,200_.jpg',
+      'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/51InjRPaF7L._SX377_BO1,204,203,200_.jpg',
+    ],
+    price: 20.11,
+    rate: 4.1,
+  },
+  {
+    title: 'JavaScript: The Good Parts: The Good Parts',
+    descripcion:
+      'Most programming languages contain good and bad parts, but JavaScript has more than its share of the bad, having been developed and released in a hurry before it could be refined. ',
+    fotos: [
+      'https://m.media-amazon.com/images/P/0596517742.01._SCLZZZZZZZ_SX500_.jpg',
+      'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/81-IfLqwLIL.jpg',
+    ],
+    price: 13.69,
+    rate: 4.8,
+  }
+);
