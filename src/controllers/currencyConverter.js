f14704fd91c26549a97a78c57e4c95f51f5d73e3
Cesar David Millan <cesardavidmillan5@gmail.com>
2023-03-21 18:38:38 -0500

    Quantity Button Done

--- src/controllers/currencyConverter.js
+++ src/controllers/currencyConverter.js
@@ -6,26 +6,33 @@ const price_id = document.getElementById('price');
 
 const valueInCOP = document.getElementById('valueInCOP');
 
-const quantity = document.getElementById('productQuantity').value;
+// const quantity = document.getElementById('productQuantity').value;
+
+const testBtn = document.getElementById('testBtn');
 
 const exchangeRate = 4500;
 
-console.log(quantity);
+let counter = 0;
 
-const pesosConverterFunction = (price_id, quantity) => {
+const pressBtn = () => {
+  return counter++;
+};
+
+testBtn.addEventListener('click', (event) => {
+  testBtn.innerText = pressBtn();
+});
+
+const pesosConverterFunction = (price_id, counter) => {
   const floatPrice = parseFloat(price_id.textContent);
-  const floatQuantity = parseFloat(quantity);
-  const pesosValue = floatPrice * exchangeRate * floatQuantity;
+  //const floatQuantity = parseFloat(quantity);
+  const pesosValue = floatPrice * exchangeRate * counter;
   return `$ ${pesosValue}`;
 };
 
+console.log(pesosConverterFunction(price_id, counter));
+
 pesosConverter.addEventListener('click', (event) => {
-  valueInCOP.innerText = pesosConverterFunction(price_id, quantity);
+  valueInCOP.innerText = pesosConverterFunction(price_id, counter);
 });
 
-function incrementValue() {
-  const value = parseInt(quantity, 10);
-  value = isNaN(value) ? 0 : value;
-  value++;
-  quantity.value = value;
-}
+//console.log(pressBtn());
