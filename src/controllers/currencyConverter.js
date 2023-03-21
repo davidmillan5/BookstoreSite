78382eafd4c880200dd3f4fccc7dec8e63be30e6
Cesar David Millan <cesardavidmillan5@gmail.com>
2023-03-21 06:51:24 -0500

    Working on how to add quantity to the function claculation

--- src/controllers/currencyConverter.js
+++ src/controllers/currencyConverter.js
@@ -6,14 +6,15 @@ const price_id = document.getElementById('price');
 
 const valueInCOP = document.getElementById('valueInCOP');
 
-const quantity = document.getElementById('quantity');
+const quantity = document.getElementById('productQuantity').value;
 
 const exchangeRate = 4500;
+console.log(quantity);
 
 const pesosConverterFunction = (price_id) => {
   const floatPrice = parseFloat(price_id.textContent);
-  const floatQuantity = parseFloat(quantity.textContent);
-  const pesosValue = floatPrice * exchangeRate;
+  const floatQuantity = parseFloat(quantity);
+  const pesosValue = floatPrice * exchangeRate * floatQuantity;
   return `$ ${pesosValue}`;
 };
 
