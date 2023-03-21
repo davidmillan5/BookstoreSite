50cae73a748265276646ee36467e720d8384e817
Cesar David Millan <cesardavidmillan5@gmail.com>
2023-03-20 22:48:23 -0500

    Adding Quantity

--- src/controllers/currencyConverter.js
+++ src/controllers/currencyConverter.js
@@ -6,10 +6,15 @@ const price_id = document.getElementById('price');
 
 const valueInCOP = document.getElementById('valueInCOP');
 
+const quantity = document.getElementById('quantity');
+
+const exchangeRate = 4500;
+
 const pesosConverterFunction = (price_id) => {
   const floatPrice = parseFloat(price_id.textContent);
-  const pesosValue = floatPrice * 4500;
-  return pesosValue;
+  const floatQuantity = parseFloat(quantity.textContent);
+  const pesosValue = floatPrice * exchangeRate;
+  return `$ ${pesosValue}`;
 };
 
 valueInCOP.innerText = pesosConverterFunction(price_id);
